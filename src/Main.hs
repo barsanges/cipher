5102ca56d3dc009415e012aef1852994bf6f4d10
barsanges <barsanges@users.noreply.github.com>
2020-07-21 15:20:40 +0200

    Create the Stack project.

---
+++ src/Main.hs
@@ -0,0 +1,13 @@
+{- |
+   Module      : Main
+   Copyright   : Copyright (C) 2020 barsanges
+   License     : GNU GPL, version 3
+
+Parses command-line options and calls the appropriate cipher.
+-}
+
+module Main where
+
+main :: IO ()
+main = do
+  undefined -- FIXME
